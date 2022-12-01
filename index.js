45c51a3239b417c807306e149b53118e75643bcf
Marc Terre <marc.terre@outlook.de>
2022-12-01 15:36:50 +0100

    delet dark mode

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// const bodyElement = document.querySelector('[data-js="body"]');
-// const buttonDarkMode = document.querySelector(
-//   '[data-js="list-button__dark-mode"]'
-// );
-// const buttonDarkModeOn = document.querySelector(
-//   '[data-js="list-button__dark-mode"]'
-// );
-// const profileBoxAboutMe = document.querySelector(
-//   '[data-js="profile-box__about-me"]'
-// );
-// const profileBoxCounters = document.querySelector(
-//   '[data-js="profile-box__counters"]'
-// );
-// const headerDarkMode = document.querySelector('[data-js="header"]');
-
-// buttonDarkMode.addEventListener("click", () => {
-//   bodyElement.classList.toggle("dark-mode");
-//   buttonDarkModeOn.classList.toggle("list-button__dark-mode");
-//   profileBoxAboutMe.classList.toggle("profile-box__about-me__dark-mode");
-//   profileBoxCounters.classList.toggle("profile-box-counters__dark-mode");
-//   headerDarkMode.classList.toggle("header__dark-mode");
-// });
-
 const bookmark = document.querySelector('[data-js="bookmarks"]');
 const answerText = document.querySelector('[data-js="answer-text"]');
 const buttonAnswer = document.querySelector('[data-js="button__answer"]');
@@ -31,7 +8,6 @@ bookmark.addEventListener("click", () => {
 });
 
 buttonAnswer.addEventListener("click", showAnswer);
-console.log(buttonAnswer);
 
 function showAnswer() {
   answerText.classList.toggle("hidden");
@@ -42,9 +18,11 @@ function showAnswer() {
   }
 }
 
-form.addEventListener("submit");
-
-function formSubmit(event) {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(event.target.value);
-}
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+});
