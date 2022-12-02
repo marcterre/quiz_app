711cdf0e572efe6bf72b137e7605436bc8847694
Marc Terre <marc.terre@outlook.de>
2022-12-02 18:00:35 +0100

    change design profile

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.clear();
+
 const bookmark = document.querySelector('[data-js="bookmarks"]');
 const answerText = document.querySelector('[data-js="answer-text"]');
 const buttonAnswer = document.querySelector('[data-js="button__answer"]');
@@ -6,11 +8,10 @@ bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("main-box-question__bookmarks-toggle");
 });
 
-buttonAnswer.addEventListener("click", showAnswer);
-
-function showAnswer(event) {
-  console.log(event);
+function toggleAnswer() {
   answerText.classList.toggle("hidden");
-  console.log(answerText);
   buttonAnswer.textContent = "hide answer";
+  console.log(buttonAnswer);
 }
+
+buttonAnswer.addEventListener("click", toggleAnswer);
